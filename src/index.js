6068f19cd13bb9f2f64c7281ac5483bb04f8217e
Temigo <temigo@gmx.com>
2018-09-21 10:52:53 -0400

    Fix scroll issue #6

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { Component }  from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from './scrollToTop.js';
+
 
 const supportsHistory = 'pushState' in window.history;
 ReactDOM.render(
   <BrowserRouter forceRefresh={!supportsHistory} keyLength={20}>
-    <App />
+    <ScrollToTop>
+      <App />
+    </ScrollToTop>
   </BrowserRouter>,
   document.getElementById('root')
 );
