535c5c59b5548fdd98c1ea05702bc52ebf4b8c6e
Temigo <temigo@gmx.com>
2018-10-19 00:54:33 -0700

    Add Google Analytics + Ribbon tutorial

--- src/index.js
+++ src/index.js
@@ -3,17 +3,28 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import ScrollToTop from './scrollToTop.js';
+import createHistory from 'history/createBrowserHistory';
+import ReactGA from 'react-ga';
 
+ReactGA.initialize('UA-127737321-1');
+
+const history = createHistory();
+//FIXME how to exclude localhost?
+history.listen(location => {
+  console.log(location);
+	ReactGA.set({ page: location.pathname })
+	ReactGA.pageview(location.pathname)
+});
 
 const supportsHistory = 'pushState' in window.history;
 ReactDOM.render(
-  <BrowserRouter forceRefresh={!supportsHistory} keyLength={20}>
+  <Router forceRefresh={!supportsHistory} keyLength={20} history={history}>
     <ScrollToTop>
       <App />
     </ScrollToTop>
-  </BrowserRouter>,
+  </Router>,
   document.getElementById('root')
 );
 registerServiceWorker();
