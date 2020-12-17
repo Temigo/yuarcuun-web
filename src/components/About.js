eecc477f29ee0599d34c9dacb3cf0c3748ff4c1a
Topher <christopher.w.liu@gmail.com>
2020-12-17 13:50:08 -0900

    changed about page

--- src/components/About.js
+++ src/components/About.js
@@ -3,23 +3,35 @@ import { Container, Header, Button, Icon, Divider } from 'semantic-ui-react';
 import '../semantic/dist/semantic.min.css';
 
 class About extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false,
+    }
+  }
   render() {
     return (
       <Container>
-        <Header as='h1'>About the Yuarcuun Initiative</Header>
+        <Header as='h1'>About Us</Header>
         <Divider />
 
-<p> Yuarcuun is a Yup'ik-led group with a mission to revitalize Eskimo/Inuit languages using computer science tools.
-
-Current contributors are Christopher Egalaaq Liu, from Mamterilleq (Bethel, Alaska) and <a href="http://temigo.github.io/">Laura Domine</a>, from Paris.
-</p>
-
-<p>We would like to properly cite and express gratitude for grammar material from the following books, "<i>A Practical Grammar of the Central Alaskan Yup'ik Eskimo Language</i>" and the "<i><a href="http://www.uaf.edu/anla/item.xml?id=CY972J2012">Yup'ik Eskimo Dictionary</a></i>" compiled by Steven Jacobson and others.</p>
-
+<p> <i>Quyana tailuci!</i> </p>
 
 <p>
-The Yuarcuun Initiative has received financial sponsorship to support a couple of ongoing projects. At the time this dictionary was implemented, the initiative was financially supported by the following entities:
+<i> Una yuarcuun website-aq computer science-aakun caliaqellruaput. Wangkuk Egalaaq Alaskuk-llu anglillruukuk Qusquqviim nuniini. Una caliaput piurtesciigacartuq ikayuqsaitellrukatkut amlleret yuut. Quyavikaput elitnauristeput, Yugtun igatulit, ilaput-llu tamalkuita.</i>
 </p>
+
+<p>
+We are proud to be a team of Yup'ik developers. Current contributors are Christopher Egalaaq Liu, from Mamterilleq (Bethel, Alaska), <a href="http://temigo.github.io/">Laura Domine</a> from Paris, and Lonny Alaskuk Strunk from Kuinerraq (Quinhagak, Alaska).
+</p>
+
+<p>We would like to properly cite and express gratitude for grammar material from the following books, "<i>A Practical Grammar of the Central Alaskan Yup'ik Eskimo Language</i>" and the "<i><a href="http://www.uaf.edu/anla/item.xml?id=CY972J2012">Yup'ik Eskimo Dictionary</a></i>" compiled by Steve Jacobson, Anna Jacobson, and others. Their work made all of this possible. <i> Quyavikamceci. </i></p>
+
+<p>
+{'The first release of this website (Oct 2018) was supported through the Yuarcuun Initiative and received financial sponsorship. At that time, the initiative was financially supported by the\xa0'}
+<span style={{textDecoration:'underline'}} onClick={()=>{this.setState({show:!this.state.show})}}>{'following entities'}</span>.
+</p>
+{this.state.show ?
 <ul>
 <li>Bethel Native Corporation</li>
 <li>Bering Straits Native Corporation</li>
@@ -28,18 +40,14 @@ The Yuarcuun Initiative has received financial sponsorship to support a couple o
 <li>Alaska Federation of Natives</li>
 <li>Alaska Native Heritage Center (Non-profit Sponsor)</li>
 </ul>
+:
+null
+}
 
-<p>Yuarcuun has many on-going plans over the next year. Please contact us at yuarcuun @ gmail dot com for questions about our projects or to be involved with any new projects on Eskimo/Inuit/Aleut languages.</p>
+<p>For this new update (Dec 2020), we integrated a <a href="https://digital.lib.washington.edu/researchworks/bitstream/handle/1773/46496/Strunk_washington_0250O_22260.pdf?sequence=1&isAllowed=y">Yugtun morphological parser</a> created by Lonny Alaskuk Strunk. Please contact us at yuarcuun @ gmail dot com for questions about our projects or to discuss any new projects.</p>
 
-<p> Quyana cakneq! </p>
+<p> <i> Quyana cakneq! </i> </p>
 
-<i>
-<p> A note from Egalaaq </p>
-<p> Quyavikanka elitnauristet Ayaprun Elitnaurvigmi, aanaka Maklak, maurluqa Narull'aq, apa'urluqa Upay'aq, alqaqa Cungass'aq, ilanka-llu tamalkuita.
-</p>
-<p> Una yuarcuun Yugtun qantulinek ciulistengqellruuq. Computer science-aakun maaggun nutarat ayuqunrilngutun elicungcaur ellimaksailan computer-aanun caliapellruapuk. Nutaranek cayarakun yugtun qaneryaramta elitnaurcuutii pilingnaqapuk. Quyana naaqiluten.
-</p>
-</i>
 
 
 
