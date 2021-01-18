f1b3bd715db5a93891d11f9e18e7255ecbedfe31
Temigo <temigo@gmx.com>
2021-01-18 15:56:19 -0800

    Start fixing circular icons bug

--- src/components/yupikEnding/YupikEndingGroups.js
+++ src/components/yupikEnding/YupikEndingGroups.js
@@ -12,46 +12,50 @@ class YupikEndingGroups extends Component {
             <List.Content floated='right'>
               <Icon color='teal' name='chevron right' />
             </List.Content>
-            <Icon circular name='help' />
+
             <List.Content verticalAlign='middle'>
+              <Icon circular name='help' />
               Question form
-
             </List.Content>
-
           </List.Item>
+
           <List.Item as={Link} to={`${this.props.match.url}/1`}>
             <List.Content floated='right'>
               <Icon color='teal' name='chevron right' />
             </List.Content>
-            <Icon circular name='talk' />
+
             <List.Content verticalAlign='middle'>
+              <Icon circular name='talk' />
               Make a command
             </List.Content>
           </List.Item>
+
           <List.Item as={Link} to={`${this.props.match.url}/2`}>
             <List.Content floated='right'>
               <Icon color='teal' name='chevron right' />
             </List.Content>
-            <Icon circular name='time' />
             <List.Content verticalAlign='middle'>
+              <Icon circular name='time' />
               Connective endings
             </List.Content>
           </List.Item>
+
           <List.Item as={Link} to={`${this.props.match.url}/3`}>
             <List.Content floated='right'>
               <Icon color='teal' name='chevron right' />
             </List.Content>
-            <Icon circular name='clone' />
             <List.Content verticalAlign='middle'>
+               <Icon circular name='clone' />
                Noun forms
             </List.Content>
           </List.Item>
+
           <List.Item as={Link} to={`${this.props.match.url}/4/postbase`}>
             <List.Content floated='right'>
               <Icon color='teal' name='chevron right' />
             </List.Content>
-            <Icon circular name='map signs' />
             <List.Content verticalAlign='middle'>
+              <Icon circular name='map signs' />
               Postbases Only
             </List.Content>
           </List.Item>
