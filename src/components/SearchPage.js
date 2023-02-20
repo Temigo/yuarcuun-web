b0b51cf1da4355fb4789d63b04a30c577a26bb69
Topher <christopher.w.liu@gmail.com>
2023-02-19 16:15:47 -0800

    removed tutorial link

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -629,7 +629,6 @@ class SearchPage extends Component {
       <Grid.Column>
       <div style={{display:'flex',justifyContent:'flex-end',paddingBottom:5}}>
       <List horizontal divided>
-        <List.Item> <a style={{textDecoration:'underline',color:'#000000de'}} href={TUTORIAL_URL} target="_blank">Tutorial</a> </List.Item>
         <List.Item> <Link style={{textDecoration:'underline',color:'#000000de'}} to='/about'>About</Link> </List.Item>
         <List.Item> <Link style={{textDecoration:'underline',color:'#000000de'}} to='/dialogues'>Dialogues</Link> </List.Item>
         <List.Item> <Link style={{textDecoration:'underline',color:'#000000de'}} to='/sentencebuilder/1'>Sentence Builder</Link> </List.Item>
