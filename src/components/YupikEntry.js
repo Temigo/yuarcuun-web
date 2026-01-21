d3876b9079204718ff6674d965c2dd43133d768f
Christopher Liu <topher@Mac.hsd1.wa.comcast.net>
2026-01-21 10:52:26 -0800

    added color when playing audio

--- src/components/YupikEntry.js
+++ src/components/YupikEntry.js
@@ -235,9 +235,9 @@ class YupikEntry extends Component {
     return <Popup
             content={
               <List style={{fontFamily:customFontFam}} divided verticalAlign='middle'>
-                {audioRetrieved.map((k)=>{
+                {audioRetrieved.map((k,kindex)=>{
                   return <ListItem style={{display:'flex',flexDirection:'row',alignItems:'center'}}>
-                          <Icon circular onClick={()=>this.repeatAudio(k['filename'], sentenceid)} style={{color:'#106181',opacity:0.9,fontSize:'20px', margin:3, cursor:'pointer'}} name='volume up' />
+                          <Button style={{float:'left', border:(this.state.playingAudio && this.state.clickedAudioIndex === kindex ? 'solid 1px #4a80b5' : 'solid 1px #dededf'),marginLeft:5,backgroundColor:'white', color:'#106181',cursor:'pointer'}} circular icon='volume up' onClick={()=>this.repeatAudio(k['filename'], kindex)} />
                           <div style={{display:'flex',flexDirection:'column',marginLeft:10,padding:'5px 0px'}}>
                             {k['gender'] != 'do_not_wish_to_say' ?
                               <div>{k['gender'].replaceAll('_',' / ')}</div>
