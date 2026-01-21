f0956d5b5b987dc8761f3f573a43b23795d0aa5a
Christopher Liu <topher@Mac.hsd1.wa.comcast.net>
2026-01-21 11:52:12 -0800

    added audio pause

--- src/components/SimpleWordBuilderUpdated.js
+++ src/components/SimpleWordBuilderUpdated.js
@@ -150,6 +150,10 @@ class SimpleWordBuilderUpdated extends Component {
       // entry:props.entry[2],
       audioParameterBool: record !== null || true ? true : false,
       englishString:'',
+      playingAudio: false,
+      clickedAudioUrl: "",
+      clickedAudio: null,
+      clickedAudioIndex:-1,
     }
   }
 
@@ -437,9 +441,9 @@ class SimpleWordBuilderUpdated extends Component {
     return <Popup
             content={
               <List style={{fontFamily:customFontFam}} divided verticalAlign='middle'>
-                {audioRetrieved.map((k)=>{
+                {audioRetrieved.map((k,kindex)=>{
                   return <ListItem style={{display:'flex',flexDirection:'row',alignItems:'center'}}>
-                          <Icon circular onClick={()=>this.repeatAudio(k['filename'])} style={{color:'#106181',opacity:0.9,fontSize:'20px', margin:3, cursor:'pointer'}} name='volume up' />
+                          <Button style={{float:'left', border:(this.state.playingAudio && this.state.clickedAudioIndex === kindex ? 'solid 1px #4a80b5' : 'solid 1px #dededf'),marginLeft:5,backgroundColor:'white', color:'#106181',cursor:'pointer'}} circular icon='volume up' onClick={()=>this.repeatAudio(k['filename'], kindex)} />
                           <div style={{display:'flex',flexDirection:'column',marginLeft:10,padding:'5px 0px'}}>
                             {k['gender'] != 'do_not_wish_to_say' ?
                               <div>{k['gender'].replaceAll('_',' / ')}</div>
@@ -479,21 +483,64 @@ class SimpleWordBuilderUpdated extends Component {
           />
   }
 
-  repeatAudio(audio, event, data) {
-    // console.log(audio)
-    if (!this.state.playingAudio) {
+  // repeatAudio(audio, event, data) {
+  //   // console.log(audio)
+  //   if (!this.state.playingAudio) {
 
-      let sound = new Audio(API_URL + "yugtunCrowdsourceAudio/" + audio);
-      this.setState({playingAudio: true});
+  //     let sound = new Audio(API_URL + "yugtunCrowdsourceAudio/" + audio);
+  //     this.setState({playingAudio: true});
 
-      sound.play()
+  //     sound.play()
 
-      sound.onended=()=>{
-        this.setState({playingAudio: false});
-      }
+  //     sound.onended=()=>{
+  //       this.setState({playingAudio: false});
+  //     }
+  //   }
+  // }
+
+  repeatAudio(audio, index, event, data) {
+
+    let audioURL = API_URL + "yugtunCrowdsourceAudio/" +  audio;
+
+    // stop audio on double click
+    if (this.state.playingAudio === true && this.state.clickedAudioUrl === audioURL) {
+      let audio = this.state.clickedAudio;
+      audio.pause();
+      audio.currentTime = 0;
+      this.setState({clickedAudio:audio, clickedAudioIndex:-1, playingAudio: false})
     }
-  }
 
+    // stop first audio then play new one
+    if (this.state.playingAudio === true && this.state.clickedAudioUrl !== audioURL) {
+      let firstAudio = this.state.clickedAudio;
+      firstAudio.pause();
+      firstAudio.currentTime = 0;
+
+      this.setState({playingAudio: true});
+      let secondAudio = new Audio(audioURL);
+      secondAudio.play();
+      this.setState({clickedAudioUrl:audioURL,  clickedAudioIndex:index, clickedAudio:secondAudio})
+      secondAudio.onended=()=>{this.setState({playingAudio: false,clickedAudioIndex:-1,})};
+    }
+
+    // play audio again
+    if (this.state.playingAudio === false && this.state.clickedAudioUrl === audioURL) {
+      let audio = this.state.clickedAudio;
+      this.setState({playingAudio: true,  clickedAudioIndex:index, });
+      audio.play();
+      audio.onended=()=>{this.setState({playingAudio: false,clickedAudioIndex:-1,})};
+    }
+
+    // play new audio
+    if (this.state.playingAudio === false && this.state.clickedAudioUrl !== audioURL) {
+      this.setState({playingAudio: true});
+      let audio = new Audio(audioURL);
+      audio.play();
+      this.setState({clickedAudioUrl:audioURL, clickedAudioIndex:index, clickedAudio:audio})
+      audio.onended=()=>{this.setState({playingAudio: false,clickedAudioIndex:-1,})};
+    }
+
+  }
 
   render() {
     console.log(this.state.englishString)
